b31d91a59c3e6a55c4657eac01ea40374f98d3a5
Vladislav <exs2199@gmail.com>
2020-07-25 21:29:04 +0300

    Fix extra buttons for closing and minimazing

--- MainInterface.py
+++ MainInterface.py
@@ -43,8 +43,8 @@ class MainApp(QtWidgets.QMainWindow, MainWindow.Ui_MainWindow):
         self.FileSelectionButton.clicked.connect(self.show_choose_file_window)
         self.NextStringButton.clicked.connect(self.pointer_inc)
         self.PreviousString.clicked.connect(self.pointer_red)
-        self.ExitButton.clicked.connect(self.closing_app)
-        self.RollUpButton.clicked.connect(self.minimize_app)
+        self.ExitButton.clicked.connect(self.close)
+        self.RollUpButton.clicked.connect(self.showMinimized)
 
     def init_helpers(self):
         self.PreviousString.setEnabled(False)
@@ -164,12 +164,6 @@ class MainApp(QtWidgets.QMainWindow, MainWindow.Ui_MainWindow):
             self.user_text.append(translate_line(self.orig_text[self.pointer]))
             self.machine_text.append(check_if_line_translated(self.orig_text[self.pointer], self.user_text[-1]))
             self.set_lines()
-
-    def closing_app(self):
-        self.close()
-
-    def minimize_app(self):
-        self.showMinimized()
 
 
 def main():
